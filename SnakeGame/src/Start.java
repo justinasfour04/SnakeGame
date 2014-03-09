1b08d3eaaa8201106699e7b39063b9797a01e090
justinasfour04 <justin.asfour@mail.mcgill.ca>
2014-03-09 13:10:14 -0400

    The start class and snake class
    Need to update them to all work together

---
+++ SnakeGame/src/Start.java
@@ -0,0 +1,68 @@
+import java.applet.Applet;
+import java.awt.Graphics;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class Start extends Applet implements Runnable, KeyListener {
+
+	@Override
+	public void start() {
+		// TODO Auto-generated method stub
+		super.start();
+	}
+
+	@Override
+	public void init() {
+		// TODO Auto-generated method stub
+		super.init();
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+
+	}
+	
+	@Override
+	public void stop() {
+		// TODO Auto-generated method stub
+		super.stop();
+	}
+
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+		super.destroy();
+	}
+
+	@Override
+	public void update(Graphics g) {
+		// TODO Auto-generated method stub
+		super.update(g);
+	}
+	
+	@Override
+	public void paint(Graphics g) {
+		// TODO Auto-generated method stub
+		super.paint(g);
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ SnakeGame/src/snakegame/Snake.java
@@ -0,0 +1,15 @@
+package snakegame;
+
+public class Snake {
+	
+	final int MOVESPEED = 5;
+	private int snakeX, snakeY, speedX, speedY, size;
+	
+	public void update() {
+		
+	}
+	
+	
+	
+	
+}
